ef4e66b8153196e96c0d512ed34c76bc3de02f19
Raajkumar Subramaniam <raajkumars@gmail.com>
2024-04-30 12:18:25 -0400

    Fixed function.js to handle glide parameter inputs

--- function.js
+++ function.js
@@ -1,14 +1,23 @@
 // The function here takes the parameters that you
 // have declared in the `glide.json` file, in the
 // same order.
-window.function = (qty, fromUnit, toUnit) => {
+window.function = (quantity, fromUnit, toUnit) => {
     // This is a simple example of a function that
     // converts units.
     //
-    // @param {number} qty The quantity to convert.
-    // @param {string} fromUnit The unit to convert from.
-    // @param {string} toUnit The unit to convert to.
+    // @param {object} qty The quantity to convert.
+    // @param {object} fromUnit The unit to convert from.
+    // @param {object} toUnit The unit to convert to.
     // @returns {number} The converted quantity.
-    const converter = new unitConverter(qty, fromUnit);
+
+    // For each parameter, its `.value` contains
+    // either its value in the type you've declared,
+    // or it's `undefined`.  This is a good place to
+    // extract the `.value`s and assign default
+    // values.
+    quantity = quantity.value ?? 0;
+    fromUnit = fromUnit.value;
+    toUnit = toUnit.value;
+    const converter = new unitConverter(quantity, fromUnit);
     return converter.as(toUnit).val();
 }
