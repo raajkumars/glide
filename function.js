27e152fd2490c316b1ebf9b373b7a0b4c32ed0dc
Raajkumar Subramaniam <raajkumars@gmail.com>
2024-04-30 11:54:56 -0400

    Updated

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 // The function here takes the parameters that you
 // have declared in the `glide.json` file, in the
 // same order.
-window.function = function (qty, fromUnit, toUnit) {
+window.function = (qty, fromUnit, toUnit) => {
     // This is a simple example of a function that
     // converts units.
     //
@@ -10,5 +10,5 @@ window.function = function (qty, fromUnit, toUnit) {
     // @param {string} toUnit The unit to convert to.
     // @returns {number} The converted quantity.
     const converter = new unitConverter(qty, fromUnit);
-    return converter.as(toUnit).val()
+    return converter.as(toUnit).val();
 }
